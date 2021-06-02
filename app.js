d30305671ab93cc3dde96afd3cf72fce9ce4be05
Samrat-14 <samratdewdrops0@gmail.com>
2021-06-02 12:19:18 +0530

    galleryfixed

--- app.js
+++ app.js
@@ -17,3 +17,34 @@ for (let i=0; i<more.length; i++){
         more[i].parentNode.classList.toggle('active')
     });
 }
+
+// achievements gallery
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let gallery = document.querySelectorAll('.gallery');
+
+let i=0;
+gallery[i].firstChild.classList.toggle('show');
+prev.classList.add('inactive');
+next.addEventListener('click', function(){
+    if(i < gallery.length - 1){
+        gallery[i].firstChild.classList.toggle('show');
+        gallery[i+1].firstChild.classList.toggle('show');
+        prev.classList.remove('inactive');
+        i++;
+    }
+    if(i == gallery.length - 1){
+        next.classList.add('inactive');
+    }
+});
+prev.addEventListener('click', function(){
+    if(i > 0){
+        gallery[i].firstChild.classList.toggle('show');
+        gallery[i-1].firstChild.classList.toggle('show');
+        next.classList.remove('inactive');
+        i--;
+    }
+    if(i == 0){
+        prev.classList.add('inactive');
+    }
+});
